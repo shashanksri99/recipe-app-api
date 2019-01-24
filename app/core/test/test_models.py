88979d57141477e7bc6cdecf674d3012f3609b94
Shashank Srivastava <shashanksri99@yahoo.com>
2019-01-24 22:14:11 +0530

    added test for user admin

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -1,17 +1,19 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTest(TestCase):
 
     def test_create_user_with_email_successful(self):
         """
-        Test creating a user with email address is test_create_user_with_email_successful
+        Test creating a user with email address is
+        test_create_user_with_email_successful
         """
         email = "testing@gmail.com"
         password = "Testing@1234"
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
 
         self.assertEqual(user.email, email)
@@ -22,10 +24,9 @@ class ModelTest(TestCase):
         Testing for the doamin name in an email address to be case insensitive
         """
         email = 'test@GMAIL.COM'
-        user = get_user_model().objects.create_user( email, 'testing123')
+        user = get_user_model().objects.create_user(email, 'testing123')
 
         self.assertEqual(user.email, email.lower())
-
 
     def test_new_user_invalid_email(self):
         """
