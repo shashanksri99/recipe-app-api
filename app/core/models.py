51fed3234bf9898aeaee987cde2cc94951c4b7a5
km <shashanksri99@yahoo.com>
2019-01-24 10:42:20 +0530

    linting correction

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import  AbstractBaseUser, \
-                                        BaseUserManager, \
+from django.contrib.auth.models import AbstractBaseUser,\
+                                        BaseUserManager,\
                                         PermissionsMixin
+
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
@@ -12,13 +14,14 @@ class UserManager(BaseUserManager):
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self._db) #This is used to save into db "_db" allows us to use multiple Database
+# This is used to save into db "_db" allows us to use multiple Database
+        user.save(using=self._db)
 
         return user
 
     def create_superuser(self, email, password):
         """
-        This function creates and sves superuser
+        This function creates and saves superuser
         """
         user = self.create_user(email, password)
         user.is_staff = True
@@ -32,10 +35,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     """
     custome user model that supports using username as email of the User
     """
-    email = models.EmailField(max_length = 256, unique = True)
-    name = models.CharField(max_length = 256)
-    is_active = models.BooleanField(default = True)
-    is_staff = models.BooleanField(default = False)
+    email = models.EmailField(max_length=256, unique=True)
+    name = models.CharField(max_length=256)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
